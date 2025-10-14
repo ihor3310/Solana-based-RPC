15a3d753213d28f5574f6e2b4ee4015e25b27dd0
rogi <96204825+ihor3310@users.noreply.github.com>
2025-10-14 13:00:32 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RPCInfoView, BundleCreateView, BundleDetailView, TransactionCreateView, CheckHistiryView
+from .views import RPCInfoView, BundleCreateView, BundleDetailView, TransactionCreateView, CheckHistoryView
 
 urlpatterns = [
     path('rpc/<str:addr>', RPCInfoView.as_view()),
-    path('history', CheckHistiryView.as_view()),
-    path('history/<int:ck>', CheckHistiryView.as_view()),
-    path('history/nt', CheckHistiryView.as_view()),
+    path('history', CheckHistoryView.as_view()),
+    path('history/<int:ck>', CheckHistoryView.as_view()),
+    path('history/nt', CheckHistoryView.as_view()),
     path('bundles', BundleCreateView.as_view()),
     path('bundles/<str:bundle_id>', BundleDetailView.as_view()),
     path('transaction', TransactionCreateView.as_view()),
