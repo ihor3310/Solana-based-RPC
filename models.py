0750430ab7576123b7177ac87496478627b36f20
rogi <96204825+ihor3310@users.noreply.github.com>
2025-10-22 00:24:22 +0300

    Add RegistrationRequest model with fields

--- models.py
+++ models.py
@@ -6,6 +6,18 @@ class HistoryOfAllRequests(models.Model):
     count = models.IntegerField(null=True)
     note = models.TextField(null=True, max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
-
+    
     def __str__(self):
         return f'{self.last_request}, {(self.created_at)}, {(self.count)}'
+
+class RegistrationRequest(models.Model):
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    
+    def __str__(self):
+        return f'{self.username}, {self.email}, {self.created_at}'
