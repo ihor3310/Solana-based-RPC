5a514e3db6a4a5e65a9a55a25a9cfa05e3cc6bf1
rogi <96204825+ihor3310@users.noreply.github.com>
2025-10-22 00:24:07 +0300

    Add new URL patterns for public and registration views

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RPCInfoView, BundleCreateView, BundleDetailView, TransactionCreateView, CheckHistoryView
+from solana_rpc import views
 
 urlpatterns = [
     path('rpc/<str:addr>', RPCInfoView.as_view()),
@@ -9,4 +10,10 @@ urlpatterns = [
     path('bundles', BundleCreateView.as_view()),
     path('bundles/<str:bundle_id>', BundleDetailView.as_view()),
     path('transaction', TransactionCreateView.as_view()),
+
+    path('', views.PublicPageView.as_view(), name='public'),
+    path('register/', views.RegisterPageView.as_view(), name='register'),
+    path('register/rgstrd/', views.RegisterPageView.as_view(), name='register_api'),
+    path('login/', views.LoginPageView.as_view(), name='login'),
+    path('home/', views.HomePageView.as_view(), name='home'),
 ]
